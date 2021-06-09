49b7d33298f07a6407da55ede7076fdcbe078168
王晨 <1016453178@qq.com>
2021-06-09 21:02:57 +0800

    整理文件

--- 用友项目/项目/配件/数据库/DCS领用汇总查询语句.sql
+++ 用友项目/项目/配件/数据库/DCS领用汇总查询语句.sql
@@ -1,3 +1,9 @@
 select * from PT_0911IR;
 
 
+select last_day(add_months(sysdate,-6))+1,last_day(add_months(sysdate,-1))  from dual;
+
+select to_char(last_day(add_months(sysdate,-6))+1,'yyyymmdd') from dual;
+
+
+delete from PT_0911IR i where i.IFRESULT = 'Z' AND substr(i.CREATETIME,0,8) = (select to_char(last_day(add_months(sysdate,-6))+1,'yyyymmdd') from dual);
