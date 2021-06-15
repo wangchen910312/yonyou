bafbdc4e6ca67ca6085c344f28a49f9a3e05bf75
王晨 <1016453178@qq.com>
2021-06-15 10:27:44 +0800

    整理文件

---
+++ 用友项目/项目/配件/刘雪莹/配件.sql
@@ -0,0 +1,5 @@
+select * from pt_0431t;--装箱单主表  ARRV_DT到达日期
+select * from pt_0432t;--装箱单明细表  GR_STRGE_CD仓库   GR_LOC_CD货位
+
+select * from pt_0621t;--外采入库，不走BMP下发
+select * from pt_0622t;--STRGE_CD编号
